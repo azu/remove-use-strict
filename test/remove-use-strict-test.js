5b5347d2dff40f337cf3aa78245d42a77a087ac2
azu <azuciao@gmail.com>
2014-03-09 16:57:35 +0900

    init

---
+++ test/remove-use-strict-test.js
@@ -0,0 +1,57 @@
+"use strict";
+var assert = require("power-assert");
+var parse = function (code) {
+    var ast = require("esprima").parse(code, { range: true, tokens: true, comment: true });
+    try {
+        ast = require("escodegen").attachComments(ast, ast.comments, ast.tokens);
+    } catch (e) {
+    }
+    return ast;
+};
+var deparse = function (ast) {
+    return require("escodegen").generate(ast, {comment: true});
+};
+var rst = require("../lib/remove-use-strict");
+describe("removeUseStrict", function () {
+    context("when does not contain use strict", function () {
+        var code = 'var a = 1;';
+        it("should return  input string", function () {
+            assert.deepEqual(rst(code), deparse(parse(code)));
+        });
+    });
+    context("when contain delective prologue string", function () {
+        it("should not remove use strict", function () {
+            var code = '"use strict"\n' +
+                'var a = 1;';
+            assert.deepEqual(rst(code), deparse(parse(code)));
+        });
+        it("should remove all use strict", function () {
+            var code = '"use strict";\n' +
+                'function a(){ "use strict"; \n var a = "use strict"; }';
+            assert.deepEqual(rst(code), deparse(parse(code)));
+        });
+    });
+    context("with comment code", function () {
+        it("should preserve coment", function () {
+            var code = '"use strict";// test';
+            assert(/test/.test(rst(code)));
+        });
+        it("should remove all unnecessary use strict", function () {
+            var code = 'var a = 1;\n' +
+                '"use strict";\n"use strict";';
+            assert.deepEqual(rst(code), 'var a = 1;');
+        });
+    });
+    context("when contain use strict which is not delective prologue", function () {
+        it("should remove unnecessary use strict", function () {
+            var code = 'var a = 1;\n' +
+                '"use strict";';
+            assert.deepEqual(rst(code), 'var a = 1;');
+        });
+        it("should remove all unnecessary use strict", function () {
+            var code = 'var a = 1;\n' +
+                '"use strict";\n"use strict";';
+            assert.deepEqual(rst(code), 'var a = 1;');
+        });
+    });
+});
